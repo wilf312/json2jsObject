68bbada2f09403032d198b0be7486b8c6bf83758
OkadaGenya <g.okada.wilf+github@gmail.com>
2018-03-02 03:46:21 +0900

    test

---
+++ src/util/__test/index.js
@@ -0,0 +1,27 @@
+import util from '../index'
+import {strictEqual} from 'assert'
+
+describe('createString', () => {
+  it('string text \'test\'', () => {
+    const value = 'test'
+    strictEqual(util.createString(value), `'${value}'`)
+  })
+})
+
+describe('createNumber', () => {
+  it('number text 123', () => {
+    const value = 123
+    strictEqual(util.createNumber(value), `${value}`)
+  })
+})
+
+describe('createBoolean', () => {
+  it('boolean text true', () => {
+    const value = true
+    strictEqual(util.createBoolean(value), `'true'`)
+  })
+  it('boolean text false', () => {
+    const value = false
+    strictEqual(util.createBoolean(value), `'false'`)
+  })
+})
