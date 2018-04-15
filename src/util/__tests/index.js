177089e121e4751f99b68c0409ff8db42d0e220f
OkadaGenya <g.okada.wilf+github@gmail.com>
2018-04-16 00:23:53 +0900

    fix

---
+++ src/util/__tests/index.js
@@ -0,0 +1,34 @@
+import * as util from '../index'
+import {strictEqual} from 'assert'
+
+describe('createString', () => {
+  it('string text \'test\'', () => {
+    const value = 'test'
+    strictEqual(util.createString(value), `'${value}'`)
+  })
+})
+
+describe('createNumber', () => {
+  it('number text 123', () => {
+    const value = 123
+    strictEqual(util.createNumber(value), `${value}`)
+  })
+})
+
+describe('test', () => {
+  it('number text 123', () => {
+    const value = 123
+    strictEqual(util.createNumber(value), `1111${value}`)
+  })
+})
+
+describe('createBoolean', () => {
+  it('boolean text true', () => {
+    const value = true
+    strictEqual(util.createBoolean(value), 'true')
+  })
+  it('boolean text false', () => {
+    const value = false
+    strictEqual(util.createBoolean(value), 'false')
+  })
+})
